1711dbf628474f27c2505efbab688846ca00774d
Jose Joaquin Rodriguez Lavalle <joaquinrodriguez70@gmail.com>
2015-03-22 00:45:25 -0600

    Comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,63 +1,68 @@
-## This are a pair of functions that cache matrices and their inverted ones
-## makeCacheMatrix creates an cached object
+## A pair of functions that cache inverted matrices
+## cacheMatrix is a vector of functions
+## cacheSolve is a cached Matrix inversion, that uses "solve"
 
-## Creates an object that hols matrices and the inverted matrix. The inverted matrix is calculated only once
-# The object returned create a list of functions that 
+## Usage example
+## myMatrix <- matrix (c(1,0,0,0,2,0,0,0,3) ,nrow=3, ncol=3)
+## cacheMatrix=makeCacheMatrix(myMatrix)
+## cacheSolve(cacheMatrix)
+
+# makeCacheMatrix
+# Creates an object that holds an inverted matrix. 
+# The object returned contains a list of functions that 
 #
-#a) sets a matrix 
-#c) gets a matrix 
-#d) gets the inverse of a matrix ( if already has calculated the matrix, if not, first inversts the matrix)
+#a) sets a matrix  (set)
+#c) gets a matrix  (get)
+#d) gets the inverse of a matrix (getInverted)
+#e) sets the inverse of a matrix (setInverted)
 
-#For example
-#myMatrix <- matrix (c(1,0,0,0,2,0,0,0,3) ,nrow=3, ncol=3)
-#myCalculation <-makeCacheMatrix(myMatrix)
-#myCalculation$get()
-#myCalculation$getInverted()
 
 makeCacheMatrix <- function(x = matrix()) {
   
-  cachedInvertedMatrix <- NULL
+  cacheInvertedMatrix <- NULL
   
   #set the matrix object
   set <- function (y) {
-    if (attributes(y)$dim[1] != attributes(y)$dim[2])  {
-      print ("Warning: Matrix is not squared!, can't be inverted")
-    }    
-    x <<- y
-    cachedInvertedMatrix <- NULL
-    
+     x <<- y
+     cacheInvertedMatrix <<- NULL
   }
+  
+  
   #get the matrix object
   get <- function() {
     x
-  }#get
+  }
   
   # Invert a matrix
   getInverted <- function() { 
 
-  # if is not a square Matrix, show an error
-  if (attributes(x)$dim[1] != attributes(x)$dim[2])  {
-      print ("Warning: Matrix is not squared!, can't be inverted")
-  } else {  
-      if ( is.null(cachedInvertedMatrix) )  {  # calculate the inverse if has not been already calculated 
-        
-        print ("calculating ...")
-        cachedInvertedMatrix <<- solve(x)
-      
-        } #if
-    
-      }#else
-    # return the (now) cached matrix
-    cachedInvertedMatrix 
+    cacheInvertedMatrix
   }#getinverted
   
-  list(set = set, get= get, getInverted = getInverted)
+  setInverted <- function(invertedM) {
+    cacheInvertedMatrix <<-invertedM
+  }
+  list(set = set, get= get,setInverted = setInverted, getInverted = getInverted)
   
 }
 
-
-## Write a short comment describing this function
+#cacheSolve
+#Receive a vector of functions (from makeCacheMatrix)
+#When called calculate the inverse of a matrix
+#The inverted matrix is calculated only once
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
+  
+  invMtrx <- x$getInverted()
+ 
+  if ( !is.null(invMtrx) )  {  # calculate the inverse if has not been already calculated 
+      print ("getting cached values ...")
+      return(invMtrx)
+    } #if
+  data <-x$get()
+  invMtrx <- solve(data,...)
+  x$setInverted(invMtrx)
+  invMtrx
+  
 }
